8273ca846811afb78f302c7780197f401ca528bd
Pablo Barrientos <pablo.barrientos@recurohealth.com>
2024-04-23 15:47:42 -0300

    first 5 lessons

---
+++ 5hours/01_init.py
@@ -0,0 +1,6 @@
+# source video : https://www.youtube.com/watch?v=t8pPdKYpowI
+# Python is a powerful language with simple syntax
+# Used in different domains such us:
+# Web (Django & Flask) / Artificial Intelligence / Data Analysis / Machine Learning / Automation
+
+print("Hello World")
---
+++ 5hours/02_num_strings.py
@@ -0,0 +1,4 @@
+# How many minutes are in 20 days?
+print(20 * 24 * 60)
+print("20 days are " + str(20 * 24 * 60) + " minutes")
+print(f"20 days are {20 * 24 * 60} minutes")
