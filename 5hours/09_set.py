74deec1f071b3b7f85669ff540e23dee60db77ed
Pablo Barrientos <pablobarrientos.cba@gmail.com>
2024-04-24 20:25:16 -0300

    sets and format string for equals

---
+++ 5hours/09_set.py
@@ -0,0 +1,10 @@
+# Store multiple values
+# does not allow duplicate values
+
+values = [1, 2, 3, 1, 4, 5, 2, 6, 7]
+
+print(f"List {values =}")
+print(f"Set {set(values) =}")
+
+print(type(values))
+print(type(set(values)))
