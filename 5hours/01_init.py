0c81e7bbcaec9a53da44f36afc0470416aebe915
Pablo Barrientos <pablo.barrientos@recurohealth.com>
2024-04-24 16:46:04 -0300

    conditionals, types

--- 5hours/01_init.py
+++ 5hours/01_init.py
@@ -4,5 +4,11 @@
 # Web (Django & Flask) / Artificial Intelligence / Data Analysis /
 # Machine Learning / Automation
 
-
 print("Hello World")
+print(type("Hello World"))
+print(type(4))
+print(type(4.56))
+print(type([1,2,3]))
+print(type((1,2,3)))
+print(type(set[1,2,3]))
+print(type(1 < 2))
