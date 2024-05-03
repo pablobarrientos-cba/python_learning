91d1f44e4ca6538c6e6910118b74055896d1cdd5
Pablo Barrientos <pablo.barrientos@recurohealth.com>
2024-05-03 16:21:53 -0300

    ZTM Python Course 40% OOP and decorators

---
+++ 01_zero_to_master/01_course_notes/OOP/02_decorators.py
@@ -0,0 +1,27 @@
+class Person:
+    species = 'human'  # class attribute
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):  # instance method can access both class and instances attributes
+        print(f'Hello, my name is: {self.name} I belong to {self.species} race')
+
+    @classmethod  # requires cls param and can access only to class attributes
+    def need_sleep(cls, hours):
+        print(f'{cls.species} need to sleep {hours} hours')
+
+    @staticmethod
+    def feed():
+        print(f'Feed')
+
+
+paul = Person('Paul', 47)
+paul.say_hello()
+paul.need_sleep(5)
+paul.feed()
+
+# not possible Person.say_hello()
+Person.need_sleep(5)
+Person.feed()
