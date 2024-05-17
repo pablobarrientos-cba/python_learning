47c278f43a219987dd0c2a640fd5ab8c84715ad8
Pablo Barrientos <pablobarrientos.cba@gmail.com>
2024-05-17 20:22:31 -0300

    100 Questions - Adding solutions for Q01

--- 02_100_questions/question_001.py
+++ 02_100_questions/question_001.py
@@ -3,7 +3,7 @@
 # between 2000 and 3200 (both included).
 # The numbers obtained should be printed in a comma-separated sequence on a single line.
 
-def get_numbers():
+def get_numbers_list():
     number_list = []
     for num in range(2000, 3201):
         if num % 7 == 0 and not num % 5 == 0:
@@ -12,4 +12,22 @@ def get_numbers():
     return number_list
 
 
-print(','.join(get_numbers()))
+def get_numbers_printed():
+    for num in range(2000, 3201):
+        if num % 7 == 0 and not num % 5 == 0:
+            print(num, end=',')
+
+
+def get_numbers_generator():
+    print(* [num for num in range(2000, 3201) if num % 7 == 0 and not num % 5 == 0], sep=',')
+
+
+# Solution 1, using a list
+print(','.join(get_numbers_list()))
+
+# Solution 2, printing directly in the for loop
+get_numbers_printed()
+
+# Solution 3, generator and list comprehension
+print('\b')
+get_numbers_generator()
