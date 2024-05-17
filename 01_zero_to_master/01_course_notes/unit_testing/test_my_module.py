6024fb92d0abb748c999fe4a90887ec7d4f053ed
Pablo Barrientos <pablobarrientos.cba@gmail.com>
2024-05-17 19:57:33 -0300

    ZTM 60% - More on Unit Testing

--- 01_zero_to_master/01_course_notes/unit_testing/test_my_module.py
+++ 01_zero_to_master/01_course_notes/unit_testing/test_my_module.py
@@ -3,16 +3,28 @@ import my_module
 
 
 class TestModule(unittest.TestCase):
+
+    def setUp(self):
+        print('\n ** Setup method... **')
+
     def test_do_stuff(self):
+        """*** Testing an integer OK ****"""
         param = 5
         expected = 25
         result = my_module.do_stuff(param)
         self.assertEqual(expected, result)
 
     def test_do_stuff2(self):
+        """**** Passing in a string, we get am error ****"""
         param = 'dsffd'
         result = my_module.do_stuff(param)
         self.assertTrue(isinstance(result, ValueError))
 
+    def tearDown(self):
+        print('\n ** Clear / Clean method... **')
 
-unittest.main()
+
+if __name__ == 'main':
+    unittest.main()
+# run the tests with this command
+# py -m unittest -v
